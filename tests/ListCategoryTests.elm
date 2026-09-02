08661ff9107338dc72a573f272ae1315b82e7cda
kuflierl <41301536+kuflierl@users.noreply.github.com>
2026-09-02 09:14:19 +0000

    test(list): move fisherman tests and add list category tests

--- tests/ListCategoryTests.elm
+++ tests/ListCategoryTests.elm
@@ -1,13 +1,12 @@
-module FishermanTests exposing (..)
+module ListCategoryTests exposing (..)
 
 import Array exposing (Array)
 import Dict
 import Expect exposing (Expectation)
 import Test exposing (Test, describe, test)
-import Logic.App.Patterns.OperatorUtils exposing (getPatternOrIotaList)
 import Logic.App.Patterns.PatternRegistry exposing (getPatternFromSignature)
 import Logic.App.Stack.EvalStack exposing (applyToStackStopAtErrorOrHalt)
-import Logic.App.Types exposing (CastingContext, Iota(..), Mishap(..))
+import Logic.App.Types exposing (CastingContext, Iota(..))
 
 emptyContext : CastingContext
 emptyContext =
@@ -17,15 +16,27 @@ emptyContext =
     , macros = Dict.empty
     }
 
+pattern : String -> Iota
+pattern sig =
+    PatternIota (getPatternFromSignature Nothing sig) False
+
 suite : Test
 suite =
-    describe "Fisherman's Gambit / Fisherman's Gambit II behavior"
-        [ test "Fisherman old behavior (positive index pulls up)" <|\_ ->
+    describe "List Category & Fisherman Tests"
+        [ test "Uniqueness Purification (unique)" <|\_ ->
             let
-                pattern sig =
-                    PatternIota (getPatternFromSignature Nothing sig) False
+                listIota =
+                    IotaList (Array.fromList [ Number 1, Number 2, Number 2, Number 3, Number 1 ])
 
-                -- Stack: [0, 10, 20, 30] -> index 0 (top) is 0, pulls item at index 0 of newStack (10) to top
+                result =
+                    applyToStackStopAtErrorOrHalt
+                        (Array.fromList [ listIota ])
+                        emptyContext
+                        (Array.fromList [ pattern "aweaqa" ])
+            in
+            Expect.equal result.stack (Array.fromList [ IotaList (Array.fromList [ Number 1, Number 2, Number 3 ]) ])
+        , test "Fisherman old behavior (positive index pulls up)" <|\_ ->
+            let
                 result =
                     applyToStackStopAtErrorOrHalt
                         (Array.fromList [ Number 0, Number 10, Number 20, Number 30 ])
@@ -35,10 +46,6 @@ suite =
             Expect.equal result.stack (Array.fromList [ Number 10, Number 20, Number 30 ])
         , test "Fisherman Copy old behavior (positive index copies up)" <|\_ ->
             let
-                pattern sig =
-                    PatternIota (getPatternFromSignature Nothing sig) False
-
-                -- Stack: [0, 10, 20, 30] -> index 0 (top) is 0, copies item at index 0 of newStack (10) to top
                 result =
                     applyToStackStopAtErrorOrHalt
                         (Array.fromList [ Number 0, Number 10, Number 20, Number 30 ])
@@ -48,10 +55,6 @@ suite =
             Expect.equal result.stack (Array.fromList [ Number 10, Number 10, Number 20, Number 30 ])
         , test "Fisherman new behavior (negative index moves top iota down)" <|\_ ->
             let
-                pattern sig =
-                    PatternIota (getPatternFromSignature Nothing sig) False
-
-                -- Stack: [-1, 10, 20, 30] -> negative index moves top iota (10) down 1 step
                 result =
                     applyToStackStopAtErrorOrHalt
                         (Array.fromList [ Number -1, Number 10, Number 20, Number 30 ])
@@ -61,10 +64,6 @@ suite =
             Expect.equal result.stack (Array.fromList [ Number 20, Number 10, Number 30 ])
         , test "Fisherman Copy new behavior (negative index copies top iota down)" <|\_ ->
             let
-                pattern sig =
-                    PatternIota (getPatternFromSignature Nothing sig) False
-
-                -- Stack: [-1, 10, 20, 30] -> negative index copies top iota (10) down 1 step
                 result =
                     applyToStackStopAtErrorOrHalt
                         (Array.fromList [ Number -1, Number 10, Number 20, Number 30 ])
